bfa960f93ae2d70f50751c29d36bc687e5e4cf5d
Tomasz Klag <tomek.klag@gmail.com>
2018-09-18 19:31:59 +0200

    Create unit test

--- TodoApp/Data/ApplicationDbContext.cs
+++ TodoApp/Data/ApplicationDbContext.cs
@@ -9,6 +9,10 @@ namespace TodoApp.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public ApplicationDbContext()
+        {
+        }
+        
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
--- TodoApp/Startup.cs
+++ TodoApp/Startup.cs
@@ -45,6 +45,7 @@ namespace TodoApp
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddScoped<ITodoItemService, TodoItemService>(); 
+            services.AddAuthentication();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
